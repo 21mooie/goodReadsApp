4c1c1d9f8e3fb625fd236192cef07f78fccd6ce8
Muata Nkosi <muatankosi1@gmail.com>
2020-08-24 12:30:14 -0400

    Adding logic to render single book in application

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -5,12 +5,12 @@ const bookRouter = express.Router();
 const books = [{
   title: 'Les Miserables',
   author: 'Victor Hugo',
-  id: 1
+  id: 0
 },
 {
   title: 'The Time Machine',
   author: 'H.G. Wells',
-  id: 2
+  id: 1
 }
 ];
 bookRouter.route('/').get((req, res) => {
@@ -22,4 +22,13 @@ bookRouter.route('/').get((req, res) => {
     });
 });
 
+bookRouter.route('/:id').get((req, res) => {
+  const { id } = req.params;
+  res.render('bookView',
+    {
+      nav: [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Authors' }],
+      title: 'Library',
+      book: books[id]
+    });
+});
 module.exports = bookRouter;
