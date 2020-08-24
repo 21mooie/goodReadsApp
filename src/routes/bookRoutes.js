f4ac81c47f4c3e4a7753329c905eac20bfbc3eec
Muata Nkosi <muatankosi1@gmail.com>
2020-08-24 18:58:19 -0400

    Use middleware to structure app and requests

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -17,20 +17,25 @@ function router(nav) {
     }());
   });
 
-  bookRouter.route('/:id').get((req, res) => {
-    (async function query() {
-      const { id } = req.params;
-      const request = new sql.Request();
-      const { recordset } = await request.input('id', sql.Int, id)
-        .query('select * from books where id = @id');
+  bookRouter.route('/:id')
+    .all((req, res, next) => {
+      (async function query() {
+        const { id } = req.params;
+        const request = new sql.Request();
+        const { recordset } = await request.input('id', sql.Int, id)
+          .query('select * from books where id = @id');
+        [req.book] = recordset;
+        next();
+      }());
+    })
+    .get((req, res) => {
       res.render('bookView',
         {
           nav,
           title: 'Library',
-          book: recordset[0]
+          book: req.book
         });
-    }());
-  });
+    });
   return bookRouter;
 }
 
