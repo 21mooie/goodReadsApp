92826a3f89ce1cd202c58fb632fdc7229a90ba83
Muata Nkosi <muatankosi1@gmail.com>
2020-08-24 12:36:56 -0400

    Pass nav to router file

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const bookRouter = require('./src/routes/bookRoutes');
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
@@ -16,11 +15,14 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+const nav = [{ link: '/books', title: 'Book' }, { link: '/authors', title: 'Author' }];
+const bookRouter = require('./src/routes/bookRoutes')(nav);
+
 app.use('/books', bookRouter);
 app.get('/', (req, res) => {
   res.render('index',
     {
-      nav: [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Authors' }],
+      nav,
       title: 'Library'
     });
 });
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,33 +2,37 @@ const express = require('express');
 
 const bookRouter = express.Router();
 
-const books = [{
-  title: 'Les Miserables',
-  author: 'Victor Hugo',
-  id: 0
-},
-{
-  title: 'The Time Machine',
-  author: 'H.G. Wells',
-  id: 1
-}
-];
-bookRouter.route('/').get((req, res) => {
-  res.render('books',
-    {
-      nav: [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Authors' }],
-      title: 'Library',
-      books
-    });
-});
+function router(nav) {
+  const books = [{
+    title: 'Les Miserables',
+    author: 'Victor Hugo',
+    id: 0
+  },
+  {
+    title: 'The Time Machine',
+    author: 'H.G. Wells',
+    id: 1
+  }
+  ];
+  bookRouter.route('/').get((req, res) => {
+    res.render('books',
+      {
+        nav,
+        title: 'Library',
+        books
+      });
+  });
 
-bookRouter.route('/:id').get((req, res) => {
-  const { id } = req.params;
-  res.render('bookView',
-    {
-      nav: [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Authors' }],
-      title: 'Library',
-      book: books[id]
-    });
-});
-module.exports = bookRouter;
+  bookRouter.route('/:id').get((req, res) => {
+    const { id } = req.params;
+    res.render('bookView',
+      {
+        nav,
+        title: 'Library',
+        book: books[id]
+      });
+  });
+  return bookRouter;
+}
+
+module.exports = router;
