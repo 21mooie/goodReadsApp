0f47fea4cee5bcf34dac269da0d545d83bc0d5f8
Muata Nkosi <muatankosi1@gmail.com>
2020-08-25 11:40:26 -0400

    Remove add book to mongodb

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -50,19 +50,6 @@ function router(nav) {
     .get((req, res) => {
       const url = 'mongodb://localhost:27017';
       const dbName = 'libraryApp';
-      (async function mongo() {
-        let client;
-        try {
-          client = await MongoClient.connect(url);
-          debug('Connected correctly to server');
-          const db = client.db(dbName);
-          const response = await db.collection('books').insertMany(books);
-          res.json(response);
-        } catch (err) {
-          debug(err.stack);
-        }
-        client.close();
-      }());
       res.send('inserting books');
     });
   return adminRouter;
