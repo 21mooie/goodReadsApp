5a0666ae13169bc80901e43d75bf65a12e736c29
Muata Nkosi <muatankosi1@gmail.com>
2020-08-25 12:26:43 -0400

    Use body-parser middleware to add form to req.body

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 const sql = require('mssql');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,6 +19,8 @@ const config = {
 sql.connect(config).catch((err) => debug(err));
 
 app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
@@ -28,9 +31,11 @@ app.set('view engine', 'ejs');
 const nav = [{ link: '/books', title: 'Book' }, { link: '/authors', title: 'Author' }];
 const bookRouter = require('./src/routes/bookRoutes')(nav);
 const adminRouter = require('./src/routes/adminRoutes')(nav);
+const authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use('/books', bookRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
 app.get('/', (req, res) => {
   res.render('index',
     {
