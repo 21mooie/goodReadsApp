f84e3a0868c315ad32d8358fd14ffbbdf97c81de
Muata Nkosi <muatankosi1@gmail.com>
2020-08-26 11:21:59 -0400

    Add Mongo logic to add user to db

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ObjectID } = require('mongodb');
+const { MongoClient } = require('mongodb');
 const debug = require('debug')('app:authRoutes');
 
 const authRouter = express.Router();
@@ -7,8 +7,33 @@ const authRouter = express.Router();
 function router() {
   authRouter.route('/signUp')
     .post((req, res) => {
-      debug(req.body);
-      res.json(req.body);
+      const { username, password } = req.body;
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+      (async function addUser() {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('Connected to server');
+          const db = client.db(dbName);
+          const col = db.collection('users');
+          const user = { username, password };
+          const results = await col.insertOne(user);
+          debug(results);
+          // login user after signing them up to db
+          req.login(results.ops[0], () => {
+            res.redirect('/auth/profile');
+          });
+        } catch (err) {
+          debug(err.stack);
+        }
+        client.close();
+      }());
+      debug(req.login);
+    });
+  authRouter.route('/profile')
+    .get((req, res) => {
+      res.json(req.user);
     });
   return authRouter;
 }
